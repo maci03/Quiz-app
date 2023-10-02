f132ef1fae37e57875ea33d997626d7d84a9029e
მაცი <93665709+maci03@users.noreply.github.com>
2023-10-02 15:51:14 +0400

    quiz.js ended

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Quiz from './Quiz';
 
 function App() {
   return (
     <div className="App">
-    
+      <Quiz />
     </div>
   );
 }
--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,39 +1,79 @@
-import React, {useState} from "react";
+// src/Quiz.js
+import React, { useState } from "react";
 
 const Quiz = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [userAnswer, setUserAnswer] = useState("");
+  const [feedback, setFeedback] = useState("");
 
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [score,setScore] = useState(0);
-    const [userAnswer, setUserAnswer] = useState("");
-    const [feedback, setFeedback] = useState("");
+  const quizQuestions = [
+    {
+      question: "What is the capital of France?",
+      options: ["Berlin", "Madrid", "Paris", "Rome"],
+      correctAnswer: "Paris",
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Venus", "Mars", "Jupiter", "Saturn"],
+      correctAnswer: "Mars",
+    },
+    // Add more questions here
+  ];
 
+  const handleOptionClick = (selectedOption) => {
+    const currentQuestion = quizQuestions[currentQuestionIndex];
 
-
-    const quizQuestions = [
-        {
-          question: "What is the capital of France?",
-          options: ["Berlin", "Madrid", "Paris", "Rome"],
-          correctAnswer: "Paris",
-        },
-        {
-          question: "Which planet is known as the Red Planet?",
-          options: ["Venus", "Mars", "Jupiter", "Saturn"],
-          correctAnswer: "Mars",
-        },
-        // დანარჩენი კითხვები
-      ];
-
-    const HandleOptionClick = (selectedOption) =>{
-        const currentQuestion = quizQuestions[currentQuestionIndex];
+    if (selectedOption === currentQuestion.correctAnswer) {
+      setScore(score + 1);
+      setFeedback("Correct!");
+    } else {
+      setFeedback(`Incorrect. The correct answer is: ${currentQuestion.correctAnswer}`);
     }
 
-    if(selectedOption === currentQuestion.correctAnswer){
-        setScore(score + 1)
-        setFeedback("Correct!");
-    }else{
-        setFeedback(`incorrect. the correct answer is: ${currentQyestion.correctAnswer} `);
-    }
     setUserAnswer(selectedOption);
+  };
 
+  const handleNextQuestion = () => {
+    if (currentQuestionIndex + 1 < quizQuestions.length) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setFeedback("");
+      setUserAnswer("");
+    } else {
+      setFeedback("Quiz Complete!");
+    }
+  };
 
-}
+  const currentQuestion = quizQuestions[currentQuestionIndex];
+
+  return (
+    <div className="quiz-container">
+      <h1>Quiz Title</h1>
+      <div className="question-container">
+        <p>{currentQuestion.question}</p>
+      </div>
+      <div className="options-container">
+        {currentQuestion.options.map((option, index) => (
+          <button
+            key={index}
+            onClick={() => handleOptionClick(option)}
+            disabled={userAnswer !== ""}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+      <div className="feedback-container">
+        <p>{feedback}</p>
+      </div>
+      <button onClick={handleNextQuestion} disabled={userAnswer === ""}>
+        Next
+      </button>
+      {currentQuestionIndex === quizQuestions.length - 1 && (
+        <p>Your Score: {score} out of {quizQuestions.length}</p>
+      )}
+    </div>
+  );
+};
+
+export default Quiz;
